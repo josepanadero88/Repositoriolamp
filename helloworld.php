876aff044aa6de3a2630637fd58420b4c772ae38
josepanadero88 <joseapanadero@gmail.com>
2025-02-24 13:47:35 +0100

    sdffdfds

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de HelloWorld</title>
+</head>
+<body>
+    
+    <?php
+        echo "Hola Mundo";
+    ?>
+
+</body>
+</html>
